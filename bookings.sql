ef2c1d6a4849030e70a7eb6cf4030a571dfa90f2
Muhammad Talha <64118988+talhawaheed152@users.noreply.github.com>
2022-03-20 17:50:03 +0500

    Add files via upload

---
+++ bookings.sql
@@ -0,0 +1,5 @@
+pk , Rana , 1/4/2022  , J3 , I2
+pk , Rana , 1/4/2022  , I2 , I1 , I4
+pk , Rana , 1/4/2022  , J2 , J1
+pk , Rana , 1/4/2022  , A1 , A2
+tiger 3 , Rana , 5/5/2022  , B2 , B3 , B2
---
+++ credentials.sql
@@ -0,0 +1,4 @@
+Rana , 5555 , rana@gmail.com
+Rahul , rahul12 , rahul112@gmail.com
+Anthony , hell0 , ant@gmail.com
+
